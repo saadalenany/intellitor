37c36e46790f1868f894ede43aba3b7517238264
saad.elanany <@Xelfire00000>
2024-10-09 22:44:11 +0300

    Update Commit - fixed service-id

--- user/src/main/java/com/intellitor/user/feign/DaoFeignClient.java
+++ user/src/main/java/com/intellitor/user/feign/DaoFeignClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "dao-service")
+@FeignClient(name = "dao")
 public interface DaoFeignClient {
 
     @GetMapping("/teacher/{id}")
